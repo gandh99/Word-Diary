b7603dbac3556f3556e3566dec1276004f903926
gandh99 <gandh99@hotmail.com>
2020-04-01 16:30:13 +0800

    Successfully connected front and backends. At the moment nothing is saved in the database yet.

--- client/src/redux/actions/authenticationActions.js
+++ client/src/redux/actions/authenticationActions.js
@@ -1,11 +1,15 @@
 import { LOGIN_USER, REGISTER_USER } from '../actionTypes'
+import axios from 'axios'
 
 export const loginUserAction = userData => ({
     type: LOGIN_USER,
     payload: userData
 })
 
-export const registerUserAction = userData => ({
-    type: REGISTER_USER,
-    payload: userData
-})
+export const registerUserAction = userData => dispatch => {
+    axios.post('/authentication/register', userData)
+        .then(res => dispatch({
+            type: REGISTER_USER,
+            payload: res.data
+        }))
+}
