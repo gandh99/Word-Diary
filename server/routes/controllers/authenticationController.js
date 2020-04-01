dacd61c1a58fa6df6497c07b1a236897460cdb6e
gandh99 <gandh99@hotmail.com>
2020-04-01 21:31:08 +0800

    Registering a user now successfully saves an entry into the db

--- server/routes/controllers/authenticationController.js
+++ server/routes/controllers/authenticationController.js
@@ -1,6 +1,16 @@
-module.exports.register = (req, res, done) => {
-    const { username, password } = req.body
-    res.status(200).send(username)
+const User = require('../../models/User')
+
+module.exports.register = async (req, res, done) => {
+    try {
+        const { username, password } = req.body
+        const user = await User.create(req.body)
+        return res.status(201).json({
+            success: true,
+            data: user
+        })
+    } catch (error) { 
+        console.log(error)
+    }
 }
 
 module.exports.login = (req, res, done) => {
