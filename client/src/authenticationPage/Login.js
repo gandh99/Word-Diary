c4c76e5c090bc0af6e5c2c52d02ffb85d3c00c41
gandh99 <gandh99@hotmail.com>
2020-04-07 22:04:02 +0800

    Added some styling to the cards

--- client/src/authenticationPage/Login.js
+++ client/src/authenticationPage/Login.js
@@ -123,4 +123,4 @@ const useStyles = makeStyles((theme) => ({
     button: {
         maginTop: '2rem'
     }
-}));   
+}))
--- client/src/diary/DiaryPost.js
+++ client/src/diary/DiaryPost.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Divider } from '@material-ui/core'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import { makeStyles } from '@material-ui/core/styles'
 
 export default function DiaryPost(props) {
+    const classes = useStyles()
+
     return (
         <Grid item xs={12} sm={6}>
             <Card style={cardStyle}>
                 <CardContent>
-                    <div>{props.post.phrase}</div>
-                    <div>{props.post.translatedPhrase}</div>
-                    <div>{props.post.note}</div>
+                    <div className={classes.phrase}>{props.post.phrase}</div>
+                    <Divider />
+                    <div className={classes.translatedPhrase}>{props.post.translatedPhrase}</div>
+                    <div className={classes.note}>{props.post.note}</div>
                 </CardContent>
             </Card>
         </Grid>
@@ -21,3 +25,17 @@ export default function DiaryPost(props) {
 const cardStyle = {
     textAlign: 'start',
 }
+
+const useStyles = makeStyles((theme) => ({
+    phrase: {
+        marginBottom: '0.5rem',
+        fontWeight: 'bold'
+    },
+    translatedPhrase: {
+        color: theme.palette.primary.dark,
+        fontWeight: 'bold'
+    },
+    note: {
+        color: theme.palette.text.secondary
+    },
+}))
