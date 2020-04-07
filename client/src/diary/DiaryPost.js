1c8a269e9701a5d464cc37ffc0db7a3441cb6bb6
gandh99 <gandh99@hotmail.com>
2020-04-07 22:28:57 +0800

    Added icons to the cards

--- client/src/diary/DiaryPost.js
+++ client/src/diary/DiaryPost.js
@@ -3,6 +3,10 @@ import { Grid, Divider } from '@material-ui/core'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import { makeStyles } from '@material-ui/core/styles'
+import StarBorderIcon from '@material-ui/icons/StarBorder'
+import StarIcon from '@material-ui/icons/Star'
+import ShareIcon from '@material-ui/icons/Share'
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutlineOutlined'
 
 export default function DiaryPost(props) {
     const classes = useStyles()
@@ -11,10 +15,17 @@ export default function DiaryPost(props) {
         <Grid item xs={12} sm={6}>
             <Card style={cardStyle}>
                 <CardContent>
+                    <div style={toolbarHeaderStyle}>
+                        <ShareIcon htmlColor={toolbarIconDefaultColor} style={toolbarIconStyle} className={classes.share} />
+                        <DeleteOutlineIcon htmlColor={toolbarIconDefaultColor} style={toolbarIconStyle} className={classes.delete} />
+                    </div>
                     <div className={classes.phrase}>{props.post.phrase}</div>
                     <Divider />
                     <div className={classes.translatedPhrase}>{props.post.translatedPhrase}</div>
                     <div className={classes.note}>{props.post.note}</div>
+                    <div style={toolbarFooterStyle}>
+                        <StarBorderIcon htmlColor={toolbarIconDefaultColor} style={toolbarIconStyle} className={classes.starUnfilled} />
+                    </div>
                 </CardContent>
             </Card>
         </Grid>
@@ -24,7 +35,27 @@ export default function DiaryPost(props) {
 
 const cardStyle = {
     textAlign: 'start',
+    position: 'relative'
 }
+
+const toolbarHeaderStyle = {
+    position: 'absolute',
+    top: '10px',
+    right: '10px',
+}
+
+const toolbarFooterStyle = {
+    position: 'absolute',
+    bottom: '10px',
+    right: '10px',
+}
+
+const toolbarIconStyle = {
+    margin: '0 0.3rem',
+    cursor: 'pointer'
+}
+
+const toolbarIconDefaultColor = '#888888'
 
 const useStyles = makeStyles((theme) => ({
     phrase: {
@@ -38,4 +69,10 @@ const useStyles = makeStyles((theme) => ({
     note: {
         color: theme.palette.text.secondary
     },
+    starUnfilled: {
+    },
+    share: {
+    },
+    delete: {
+    }
 }))
