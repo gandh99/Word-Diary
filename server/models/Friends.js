2fb087bb4b681e6117db0258bfbee6c1b0cded2c
gandh99 <gandh99@hotmail.com>
2020-04-10 11:34:32 +0800

    Modified User model and created a Friends model.

---
+++ server/models/Friends.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+module.exports = statusTypes = {
+    REQUESTED: 'requested',
+    PENDING: 'pending',
+    FRIENDS: 'friends',
+}
+
+const FriendsSchema = new Schema({
+    requester: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    recipient: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    status: {
+        type: String,
+        enums: [
+            statusTypes.REQUESTED,
+            statusTypes.PENDING,
+            statusTypes.FRIENDS
+        ]
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Friends = mongoose.model('Friends', FriendsSchema)
--- server/models/User.js
+++ server/models/User.js
@@ -15,6 +15,10 @@ const UserSchema = new Schema({
         required: true,
         default: "Hi there! I'm using Word Diary!"
     },
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Friends'
+    }],
     dateCreated: {
         type: Date,
         default: Date.now
